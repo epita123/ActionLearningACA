93fdf09415e316b7659145ebc14b395e903c02ca
Android <chandanapanchumarthi@gmail.com>
2022-07-26 17:53:02 +0200

    Professor class

---
+++ src/main/java/fr/epita/datamodel/Prof.java
@@ -0,0 +1,14 @@
+package fr.epita.datamodel;
+
+public class Prof {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String name;
+
+}
